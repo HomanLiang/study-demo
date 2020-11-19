56975acad669a8933bc55f7ff80084afdcacce7c
Homan <lianghaoming@allchips.com>
2020-11-19 14:56:13 +0800

    BIO服务器端Demo

--- netty-demo/src/main/java/com/homan/bio/BIOServer.java
+++ netty-demo/src/main/java/com/homan/bio/BIOServer.java
@@ -24,7 +24,7 @@ public class BIOServer {
         // 1. 创建一个线程池
         // 2. 如果有客户端连接，就创建一个线程，与之通讯(单独写一个方法)
         ExecutorService newCachedThreadPool = Executors.newCachedThreadPool();
-        //创建ServerSocket
+        // 创建ServerSocket
         ServerSocket serverSocket = new ServerSocket(6666);
         System.out.println("服务器启动了");
 
@@ -36,8 +36,9 @@ public class BIOServer {
             System.out.println("连接到一个客户端");
             // 就创建一个线程，与之通讯(单独写一个方法)
             newCachedThreadPool.execute(new Runnable() {
-                public void run() { //我们重写
-                    //可以和客户端通讯
+                // 我们重写
+                public void run() {
+                    // 可以和客户端通讯
                     handler(socket);
                 }
             });
@@ -52,17 +53,17 @@ public class BIOServer {
         try {
             System.out.println("线程信息 id =" + Thread.currentThread().getId() + " 名字=" + Thread.currentThread().getName());
             byte[] bytes = new byte[1024];
-            //通过socket 获取输入流
+            // 通过socket 获取输入流
             InputStream inputStream = socket.getInputStream();
 
-            //循环的读取客户端发送的数据
+            // 循环的读取客户端发送的数据
             while (true) {
                 System.out.println("线程信息 id =" + Thread.currentThread().getId() + " 名字=" + Thread.currentThread().getName());
                 System.out.println("read....");
                 int read = inputStream.read(bytes);
                 if (read != -1) {
-                    System.out.println(new String(bytes, 0, read
-                    )); //输出客户端发送的数据
+                    // 输出客户端发送的数据
+                    System.out.println(new String(bytes, 0, read));
                 } else {
                     break;
                 }
